6b94bfa66a4ecea1961831dc98d6b9923fca89de
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2020-12-10 23:32:51 -0600

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,21 @@
+def my_collect(collection)
+    i = 0 
+    new_collection = []
+    while i < collection.length
+        new_collection << yield(collection[i])
+        i += 1
+    
+    end
+
+new_collection
+end
+
+# my_collect(collection) do |item|
+#     item.upcase.split(" ").first
+# end
+
+
+
+
 
 
